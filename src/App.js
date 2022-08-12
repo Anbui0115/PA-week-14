6a1f129d338b124ca1fd0fe6f9d93247e34bb8a3
An Bui <dobui0115@gmail.com>
2022-08-12 16:09:08 -0700

    solved phase 5

--- src/App.js
+++ src/App.js
@@ -3,14 +3,32 @@ import FruitsIndex from "./components/FruitsIndex";
 import FruitForm from "./components/FruitForm";
 import FavoriteFruit from "./components/FavoriteFruit";
 import SetFavoriteFruit from "./components/SetFavoriteFruit";
+import { Route, Switch } from "react-router-dom";
+import FruitShow from "./components/FruitShow";
 function App() {
   return (
     <>
       <h1>Welcome to Fruits App</h1>
-      <FruitsIndex fruits={fruits} />
-      <FruitForm fruits={fruits} />
-      <FavoriteFruit fruits={fruits} />
-      <SetFavoriteFruit fruits={fruits} />
+      <Switch>
+        <Route exact path={"/"}>
+          <FruitsIndex fruits={fruits} />
+        </Route>
+        <Route path={"/fruits/new"}>
+          <FruitForm fruits={fruits} />
+        </Route>
+        <Route path={"/fav-fruit"}>
+          <FavoriteFruit fruits={fruits} />
+        </Route>
+        <Route path={"/set-fav-fruit"}>
+          <SetFavoriteFruit fruits={fruits} />
+        </Route>
+        <Route path={"/fruits/:fruitId"}>
+          <FruitShow fruits={fruits} />
+        </Route>
+        <Route>
+          <div>Page Not Found</div>
+        </Route>
+      </Switch>
     </>
   );
 }
