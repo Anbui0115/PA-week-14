32c2c45e68f1d1bd5c15c8ac93f346d24cdfc0c0
An Bui <dobui0115@gmail.com>
2022-08-12 14:02:52 -0700

    solved phase 4

--- src/components/SetFavoriteFruit.js
+++ src/components/SetFavoriteFruit.js
@@ -1,15 +1,18 @@
+import { useContext } from "react";
+import { FavFruitContext } from "../context/FavFruitContext";
+
 const SetFavoriteFruit = ({ fruits }) => {
+  const { favFruitId, setFavFruitId } = useContext(FavFruitContext);
   return (
     <div className="set-fav-fruit">
       <h2>Select your Favorite Fruit</h2>
       <label>
         <select
+          value={favFruitId}
+          onChange={(e) => setFavFruitId(e.target.value)}
         >
-          {fruits.map(fruit => (
-            <option
-              key={fruit.id}
-              value={fruit.id}
-            >
+          {fruits.map((fruit) => (
+            <option key={fruit.id} value={fruit.id}>
               {fruit.name}
             </option>
           ))}
@@ -17,6 +20,6 @@ const SetFavoriteFruit = ({ fruits }) => {
       </label>
     </div>
   );
-}
+};
 
 export default SetFavoriteFruit;
